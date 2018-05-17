3125dd89e609fa1dc16cfa14d4a5521b82e5efdf
greywolf1991 <spellbuilder1991@gmail.com>
2018-05-17 23:02:17 +0300

    Initial commit

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	double first_year = 200;
+	for (int i = 0; i < 12; ++i)
+        first_year = first_year + first_year * 0.0661 / 12 + 30;
+
+	cout << "first_year: " << first_year  << endl;
+
+    double second_year = first_year;
+    for (int i = 0; i < 12; ++i)
+        second_year = second_year + second_year * 0.0661 / 12 + 9;
+    cout << "second_year: "<< second_year << endl;
+	
+    /*for (int i = 1; i <= 100; i++)
+    {
+        if ((i % 3 == 0) && (i % 5 == 0)) std::cout << "FizzBuzz" << std::endl;
+        else if (i % 3 == 0) std::cout << "Fizz" << std::endl;
+        else if (i % 5 == 0) std::cout << "Buzz" << std::endl;
+        else    std::cout << i << std::endl;
+    }*/
+    system("pause");
+	return 0;
+}
